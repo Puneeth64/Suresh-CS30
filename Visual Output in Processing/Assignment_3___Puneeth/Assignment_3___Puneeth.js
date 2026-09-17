ad90b34b447c1054fa758638498ddd404d47c008
Puneeth64 <96257382+Puneeth64@users.noreply.github.com>
2026-09-17 08:54:22 -0600

    Add files via upload

---
+++ Visual Output in Processing/Assignment_3___Puneeth/Assignment_3___Puneeth.js
@@ -0,0 +1,15 @@
+/* Puneeth Suresh, 2107296@gscs.ca, Assignment  */
+
+function setup() {
+  /*First name */
+text("Puneeth Suresh",0,10);
+  /*School email*/
+text("2107296@gscs.ca",0,30);
+  /* Grade */
+text("hopefully over 90%",0,50);
+
+}
+
+function draw() {
+
+}
